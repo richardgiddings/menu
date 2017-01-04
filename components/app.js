4d7e0bbfb505d4308cbde0d809d3fda51faf6f6f
Richard Giddings <rgiddings20@hotmail.com>
2017-01-04 17:09:31 +0000

    Separate App component and include it in index.js.

---
+++ components/app.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import Menu from '../components/menu';
+
+import Home from '../pages/home';
+import Contact from '../pages/contact';
+import Links from '../pages/links';
+import Recipes from '../pages/recipes';
+
+var Pages = { Home, Contact, Links, Recipes };
+
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            currentItem: "Home"
+        };
+        this.menuItemChange = this.menuItemChange.bind(this);
+    }
+
+    menuItemChange(selectedItem) {
+        this.setState({
+            currentItem: selectedItem
+        });
+    }
+
+    render() {
+        const Content = Pages[this.state.currentItem];
+
+        return (
+            <div>
+                <Menu onMenuChange={this.menuItemChange} />
+                <Content className="content" />
+            </div>
+        );
+    }
+}
